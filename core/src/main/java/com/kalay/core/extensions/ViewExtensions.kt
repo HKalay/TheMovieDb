b53449c3e63ea39358280aa9a6f02e9efc2c71fb
hamdullahkalay16 <hamdullahkly@gmail.com>
2021-01-31 20:08:53 +0300

    Refactoring modules.

--- core/src/main/java/com/kalay/core/extensions/ViewExtensions.kt
+++ core/src/main/java/com/kalay/core/extensions/ViewExtensions.kt
@@ -1,6 +1,8 @@
 package com.kalay.core.extensions
 
 import android.view.View
+import android.widget.ImageView
+import com.squareup.picasso.Picasso
 
 fun View.gone() {
     visibility = View.GONE
@@ -8,4 +10,10 @@ fun View.gone() {
 
 fun View.visibile() {
     visibility = View.VISIBLE
+}
+
+fun ImageView.loadImage(url: String) {
+    Picasso.with(this.context)
+        .load(url)
+        .into(this)
 }
